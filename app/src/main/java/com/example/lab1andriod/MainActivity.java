e7e583d72b6415ba3b4aa1a5525fc73ed9df45f0
codes-easy <116019724+codes-easy@users.noreply.github.com>
2022-11-14 14:04:02 -0500

    first

--- app/src/main/java/com/example/lab1andriod/MainActivity.java
+++ app/src/main/java/com/example/lab1andriod/MainActivity.java
@@ -2,34 +2,103 @@ package com.example.lab1andriod;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+        implements View.OnClickListener
+{
+    int n1 = 0; int n2 =0; int n3 =0; int n4 =0;
+    EditText show_num;
+    Button Number1; Button Number2; Button Number3; Button Number4;
+    Button Number5; Button Number6; Button Number7; Button Number8;
+    Button Number9; Button Number0; Button addbut;Button subbut;
+    Button timesbut;Button divbut;Button clearbut;Button equalbut;
+    Button HistoryStd;
+    Button HistoryAdv;
 
+    @SuppressLint({"WrongViewCast", "MissingInflatedId"})
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        int n1 = 0; int n2 =0; int n3 =0; int n4 =0;
-        EditText resultext;
-        Button Number1; Button Number2; Button Number3; Button Number4;
-        Button Number5; Button Number6; Button Number7; Button Number8;
-        Button Number9; Button Number0; Button addbut;Button subbut;
-        Button timesbut;Button divbut;Button clearbut;Button Num0;
-        Button HistoryStd;
-        Button HistoryAdv;
-
-
-
-
-
-
-
-
-
-
-        super.onCreate(savedInstanceState);
+       super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        show_num = findViewById(R.id.shownumbers);
+        Number1 = findViewById(R.id.Number1); Number2 = findViewById(R.id.Number2);
+        Number3 = findViewById(R.id.Number3); Number4 = findViewById(R.id.Number4);
+        Number5 = findViewById(R.id.Number5); Number6 = findViewById(R.id.Number6);
+        Number7 = findViewById(R.id.Number7); Number8 = findViewById(R.id.Number8);
+        Number9 = findViewById(R.id.Number9); Number0 = findViewById(R.id.Number0);
+       addbut = findViewById(R.id.addbutton); subbut = findViewById(R.id.subbutton);
+       divbut = findViewById(R.id.divisionbutton); timesbut = findViewById(R.id.timesbutton);
+       clearbut = findViewById(R.id.clearbutton); equalbut = findViewById(R.id.equalbutton);
+       HistoryStd = findViewById(R.id.StdNoHistory);
+
+        Number1.setOnClickListener(this); Number2.setOnClickListener(this);
+        Number3.setOnClickListener(this); Number4.setOnClickListener(this);
+        Number5.setOnClickListener(this); Number6.setOnClickListener(this);
+        Number7.setOnClickListener(this); Number8.setOnClickListener(this);
+        Number9.setOnClickListener(this); Number0.setOnClickListener(this);
+        addbut.setOnClickListener(this); subbut.setOnClickListener(this);
+        timesbut.setOnClickListener(this); divbut.setOnClickListener(this);
+        equalbut.setOnClickListener(this); clearbut.setOnClickListener(this);
+        HistoryStd.setOnClickListener(this); show_num.setOnClickListener(this);
+
+
+    }
+
+    @Override
+    public void onClick(View view) {
+      switch (view.getId()){
+          case R.id.Number0:
+              show_num.setText("0");
+              break;
+
+              case R.id.Number1:
+                  show_num.setText("1");
+                  break;
+
+          case R.id.Number2:
+              show_num.setText("2");
+              break;
+
+          case R.id.Number3:
+              show_num.setText("3");
+              break;
+
+          case R.id.Number4:
+              show_num.setText("4");
+              break;
+          case R.id.Number5:
+              show_num.setText("5");
+              break;
+          case R.id.Number6:
+              show_num.setText("6");
+              break;
+          case R.id.Number7:
+              show_num.setText("7");
+              break;
+
+          case R.id.Number8:
+              show_num.setText("8");
+              break;
+
+          case R.id.Number9:
+              show_num.setText("9");
+              break;
+
+          case R.id.addbutton:
+              show_num.setText("+");
+              break;
+
+
+
+
+      }
+
     }
 }
