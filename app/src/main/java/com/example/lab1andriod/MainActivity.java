372824057f147e129cdb078365d795d721dd321e
Chinmay Shah <chinmay.shah@appliedsystems.com>
2022-11-14 02:06:16 -0500

    UI done

--- app/src/main/java/com/example/lab1andriod/MainActivity.java
+++ app/src/main/java/com/example/lab1andriod/MainActivity.java
@@ -3,11 +3,32 @@ package com.example.lab1andriod;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        int n1 = 0; int n2 =0; int n3 =0; int n4 =0;
+        EditText resultext;
+        Button Number1; Button Number2; Button Number3; Button Number4;
+        Button Number5; Button Number6; Button Number7; Button Number8;
+        Button Number9; Button Number0; Button addbut;Button subbut;
+        Button timesbut;Button divbut;Button clearbut;Button Num0;
+        Button HistoryStd;
+        Button HistoryAdv;
+
+
+
+
+
+
+
+
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
---
+++ app/src/main/java/com/example/lab1andriod/calculate.java
@@ -0,0 +1,4 @@
+package com.example.lab1andriod;
+
+public class calculate {
+}
