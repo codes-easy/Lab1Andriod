82bde03d51c3801f51a40f077660ca142c019b10
codes-easy <116019724+codes-easy@users.noreply.github.com>
2022-11-15 16:49:13 -0500

    show buttons

--- app/src/main/java/com/example/lab1andriod/MainActivity.java
+++ app/src/main/java/com/example/lab1andriod/MainActivity.java
@@ -8,75 +8,143 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity
-        implements View.OnClickListener
-{
+        implements View.OnClickListener {
 
     TextView show_num;
-    Button Number1; Button Number2; Button Number3; Button Number4;
-    Button Number5; Button Number6; Button Number7; Button Number8;
-    Button Number9; Button Number0; Button addbut;Button subbut;
-    Button timesbut;Button divbut;Button clearbut;Button equalbut;
+    Button Number1;
+    Button Number2;
+    Button Number3;
+    Button Number4;
+    Button Number5;
+    Button Number6;
+    Button Number7;
+    Button Number8;
+    Button Number9;
+    Button Number0;
+    Button addbut;
+    Button subbut;
+    Button timesbut;
+    Button divbut;
+    Button clearbut;
+    Button equalbut;
     Button HistoryStd;
     Button HistoryAdv;
     calculate calc = new calculate();
+    String dataToCalculate = "";
+    boolean flag = true;
+
 
     @SuppressLint({"WrongViewCast", "MissingInflatedId"})
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-       super.onCreate(savedInstanceState);
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         show_num = findViewById(R.id.shownumbers);
-        Number1 = findViewById(R.id.Number1); Number2 = findViewById(R.id.Number2);
-        Number3 = findViewById(R.id.Number3); Number4 = findViewById(R.id.Number4);
-        Number5 = findViewById(R.id.Number5); Number6 = findViewById(R.id.Number6);
-        Number7 = findViewById(R.id.Number7); Number8 = findViewById(R.id.Number8);
-        Number9 = findViewById(R.id.Number9); Number0 = findViewById(R.id.Number0);
-       addbut = findViewById(R.id.addbutton); subbut = findViewById(R.id.subbutton);
-       divbut = findViewById(R.id.divisionbutton); timesbut = findViewById(R.id.timesbutton);
-       clearbut = findViewById(R.id.clearbutton); equalbut = findViewById(R.id.equalbutton);
-       HistoryStd = findViewById(R.id.StdNoHistory);
+        Number1 = findViewById(R.id.Number1);
+        Number2 = findViewById(R.id.Number2);
+        Number3 = findViewById(R.id.Number3);
+        Number4 = findViewById(R.id.Number4);
+        Number5 = findViewById(R.id.Number5);
+        Number6 = findViewById(R.id.Number6);
+        Number7 = findViewById(R.id.Number7);
+        Number8 = findViewById(R.id.Number8);
+        Number9 = findViewById(R.id.Number9);
+        Number0 = findViewById(R.id.Number0);
+        addbut = findViewById(R.id.addbutton);
+        subbut = findViewById(R.id.subbutton);
+        divbut = findViewById(R.id.divisionbutton);
+        timesbut = findViewById(R.id.timesbutton);
+        clearbut = findViewById(R.id.clearbutton);
+        equalbut = findViewById(R.id.equalbutton);
+        HistoryStd = findViewById(R.id.StdNoHistory);
 
-        Number1.setOnClickListener(this); Number2.setOnClickListener(this);
-        Number3.setOnClickListener(this); Number4.setOnClickListener(this);
-        Number5.setOnClickListener(this); Number6.setOnClickListener(this);
-        Number7.setOnClickListener(this); Number8.setOnClickListener(this);
-        Number9.setOnClickListener(this); Number0.setOnClickListener(this);
-        addbut.setOnClickListener(this); subbut.setOnClickListener(this);
-        timesbut.setOnClickListener(this); divbut.setOnClickListener(this);
-        equalbut.setOnClickListener(this); clearbut.setOnClickListener(this);
-        HistoryStd.setOnClickListener(this); show_num.setOnClickListener(this);
+        Number1.setOnClickListener(this);
+        Number2.setOnClickListener(this);
+        Number3.setOnClickListener(this);
+        Number4.setOnClickListener(this);
+        Number5.setOnClickListener(this);
+        Number6.setOnClickListener(this);
+        Number7.setOnClickListener(this);
+        Number8.setOnClickListener(this);
+        Number9.setOnClickListener(this);
+        Number0.setOnClickListener(this);
+        addbut.setOnClickListener(this);
+        subbut.setOnClickListener(this);
+        timesbut.setOnClickListener(this);
+        divbut.setOnClickListener(this);
+        equalbut.setOnClickListener(this);
+        clearbut.setOnClickListener(this);
+        HistoryStd.setOnClickListener(this);
+        show_num.setOnClickListener(this);
 
 
     }
+
     boolean isNewOperator = true;
+
     @Override
     public void onClick(View view) {
         Button button = (Button) view;
         String buttonText = button.getText().toString();
-        String dataToCalculate = show_num.getText().toString();
-       // show_num.setText(buttonText);
 
-        if(isNewOperator)
-            show_num.setText("");
-        isNewOperator =false;
+        //String dataToCalculate = show_num.getText().toString();
+        // show_num.setText(buttonText);
 
-        if(buttonText.equals("C")) {
+        if (isNewOperator)
             show_num.setText("");
-            dataToCalculate="";
+        isNewOperator = false;
+
+        if (buttonText.equals("C")) {
+            flag = true;
+            show_num.setText("");
+            dataToCalculate = "";
+
+            //also this is used
+            calc.fn.clear();
+            calc.sm.clear();
+            calc.result = 0;
+
             return;
-        }
-        if(buttonText.equals("=")){
+        } else if (buttonText.equals("=")) {
             //show_num.setText(show_num.getText().toString()+"=");
 
-            show_num.setText(String.valueOf(calc.calculatefn(buttonText)));
-        }
-        else {
-            dataToCalculate = dataToCalculate + buttonText;
+            show_num.setText(dataToCalculate + "=" + String.valueOf(calc.calculatefn(buttonText)));
+        } else {
+            if (buttonText.equals("1") || buttonText.equals("2") || buttonText.equals("3") ||
+                    buttonText.equals("4") || buttonText.equals("5") || buttonText.equals("6") ||
+                    buttonText.equals("7") || buttonText.equals("8") || buttonText.equals("9") ||
+                    buttonText.equals("0") ) {
+                if (flag) {
+                    dataToCalculate = dataToCalculate + buttonText;
+                    show_num.setText(dataToCalculate);
+                    calc.calculatefn(buttonText);
+                    flag = false;
+                } else {
+                    Toast.makeText(MainActivity.this, "ADD OPERATOR", Toast.LENGTH_SHORT).show();
+                }
+            } else if (buttonText.equals("+") || buttonText.equals("*") || buttonText.equals("/")|| buttonText.equals("-")) {
+                if (!flag) {
+                    dataToCalculate = dataToCalculate + buttonText;
+                    show_num.setText(dataToCalculate);
+                    calc.calculatefn(buttonText);
+                    flag = true;
+                } else {
+                    Toast.makeText(MainActivity.this, "ADD number", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+
+
+
+         /*   dataToCalculate = dataToCalculate + buttonText;
             show_num.setText(dataToCalculate);
-            calc.calculatefn(buttonText);
+            calc.calculatefn(buttonText);*/
         }
 
 
@@ -131,9 +199,6 @@ public class MainActivity extends AppCompatActivity
 //              break;
 
 
-
-
-
-      }
-
     }
+
+}
